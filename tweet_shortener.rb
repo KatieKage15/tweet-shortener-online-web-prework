19dc47c4a9d3f18d0d5b410b36f00caffebdbacf
victorious-messaging-4309 <wildayk@gmail.com>
2019-03-17 19:49:16 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -33,15 +33,13 @@ def selective_tweet_shortener(tweet)
     word_substituter(tweet)
   else
     tweet
-  end  
+  end
 end
 
 def shortened_tweet_truncator(tweet)
   if word_substituter(tweet).length > 140
-    word_substituter(tweet)[0..136] + '...' 
+    word_substituter(tweet)[0..136] + '...'
   else
     tweet
   end
 end
-
-  
